2f0c60a3bb1f6e7b18bd13854aef63b6459d39e4
huwuxtins <nguyenhuutin124@gmail.com>
2024-11-09 21:42:20 +0700

    feat(user): add user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 // DatabaseModule
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
